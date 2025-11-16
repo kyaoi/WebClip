3bffb57e414fecd7f82594cd571f3c9bb9fcf1e7
kyaoi <aoistudy90@gmail.com>
2025-11-16 14:55:46 +0900

    feat: version 1.1.0

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "WebClip",
-  version: "1.0.0",
+  version: "1.1.0",
   description:
     "選択したテキストをMarkdownファイルへローカル保存する、オフライン専用のクリッピング拡張です。",
   background: {
